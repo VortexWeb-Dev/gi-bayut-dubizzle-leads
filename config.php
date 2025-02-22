d5f1d47c7da680c32a686401ec7079d8423cc476
fasilofficial <muhammedfasilofficial@gmail.com>
2025-02-22 16:33:47 +0530

    Update fallback responsible person

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ define('AUTH_TOKEN', "d7db72b0f9f083fe3c33441412a47e1e");
 define('TIMESTAMP', date("Y-m-d H:i:s")); // Production
 // define('TIMESTAMP', "2025-02-21 00:00:00"); // Testing
 define('LEAD_FILE', __DIR__ . '/processed_leads.txt');
-define('DEFAULT_ASSIGNED_USER_ID', '1893'); // Justine Panganiban
+define('DEFAULT_ASSIGNED_USER_ID', '1593'); // Alyssa Ashley Erise
 define('SECONDARY_PIPELINE_ID', 24);
